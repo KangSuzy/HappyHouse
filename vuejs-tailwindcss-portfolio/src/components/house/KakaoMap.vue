e852887933aa9b4918b20f63b9ae2cb118dbc3c4
강수지 <astnwl321@gmail.com>
2022-11-21 17:52:54 +0900

    [2022-11-21 강수지] House 마커 찍어야된다 . . .

--- vuejs-tailwindcss-portfolio/src/components/house/KakaoMap.vue
+++ vuejs-tailwindcss-portfolio/src/components/house/KakaoMap.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <div id="map"></div>
-    <!-- <div class="button-group">
-      <button @click="changeSize(0)">Hide</button>
-      <button @click="changeSize(400)">show</button>
-      <button @click="displayMarker(markerPositions1)">marker set 1</button>
-      <button @click="displayMarker(markerPositions2)">marker set 2</button>
-      <button @click="displayMarker([])">marker set 3 (empty)</button>
-      <button @click="displayInfoWindow">infowindow</button>
-    </div> -->
+    <div class="button-group">
+      <!-- <button @click="changeSize(0)">Hide</button>
+      <button @click="changeSize(400)">show</button> -->
+      <button @click="displayMarker(markerPositions)">marker set 1</button>
+      <!-- <button @click="displayMarker(markerPositions2)">marker set 2</button> -->
+      <!-- <button @click="displayMarker([])">marker set 3 (empty)</button> -->
+      <!-- <button @click="displayInfoWindow">infowindow</button> -->
+    </div>
   </div>
 </template>
 
@@ -17,20 +17,13 @@ export default {
   name: "KakaoMap",
   data() {
     return {
-      // markerPositions1: [
-      //   [33.452278, 126.567803],
-      //   [33.452671, 126.574792],
-      //   [33.451744, 126.572441],
-      // ],
-      // markerPositions2: [
-      //   [37.499590490909185, 127.0263723554437],
-      //   [37.499427948430814, 127.02794423197847],
-      //   [37.498553760499505, 127.02882598822454],
-      //   [37.497625593121384, 127.02935713582038],
-      //   [37.49629291770947, 127.02587362608637],
-      //   [37.49754540521486, 127.02546694890695],
-      //   [37.49646391248451, 127.02675574250912],
-      // ],
+      markerPositions: [
+        // 선택한 . . . 집의 좌표값을 넣어주면 될 듯 ?!
+        [37.5743822, 126.9688505],
+        [37.57348, 126.967792],
+        // [33.451744, 126.572441],
+      ],
+      
       markers: [],
       infowindow: null,
     };
@@ -65,32 +58,32 @@ export default {
     //   container.style.height = `${size}px`;
     //   this.map.relayout();
     // },
-    // displayMarker(markerPositions) {
-    //   if (this.markers.length > 0) {
-    //     this.markers.forEach((marker) => marker.setMap(null));
-    //   }
+    displayMarker(markerPositions) {
+      if (this.markers.length > 0) {
+        this.markers.forEach((marker) => marker.setMap(null));
+      }
 
-    //   const positions = markerPositions.map(
-    //     (position) => new kakao.maps.LatLng(...position)
-    //   );
+      const positions = markerPositions.map(
+        (position) => new kakao.maps.LatLng(...position)
+      );
 
-    //   if (positions.length > 0) {
-    //     this.markers = positions.map(
-    //       (position) =>
-    //         new kakao.maps.Marker({
-    //           map: this.map,
-    //           position,
-    //         })
-    //     );
+      if (positions.length > 0) {
+        this.markers = positions.map(
+          (position) =>
+            new kakao.maps.Marker({
+              map: this.map,
+              position,
+            })
+        );
 
-    //     const bounds = positions.reduce(
-    //       (bounds, latlng) => bounds.extend(latlng),
-    //       new kakao.maps.LatLngBounds()
-    //     );
+        const bounds = positions.reduce(
+          (bounds, latlng) => bounds.extend(latlng),
+          new kakao.maps.LatLngBounds()
+        );
 
-    //     this.map.setBounds(bounds);
-    //   }
-    // },
+        this.map.setBounds(bounds);
+      }
+    },
     // displayInfoWindow() {
     //   if (this.infowindow && this.infowindow.getMap()) {
     //     //이미 생성한 인포윈도우가 있기 때문에 지도 중심좌표를 인포윈도우 좌표로 이동시킨다.
