e71a327f2fdb152b70c19b111785a329ec044a30
강수지 <astnwl321@gmail.com>
2022-11-23 15:08:23 +0900

    [2022-11-23 강수지] 총 기능 완성 !

--- vuejs-tailwindcss-portfolio/src/components/about/AboutClients.vue
+++ vuejs-tailwindcss-portfolio/src/components/about/AboutClients.vue
@@ -1,77 +1,75 @@
 <script>
+import { defineComponent } from "vue";
+import { Carousel, Pagination, Slide } from "vue3-carousel";
 
-import { defineComponent } from 'vue'
-import { Carousel, Pagination, Slide } from 'vue3-carousel'
-
-import 'vue3-carousel/dist/carousel.css'
+import "vue3-carousel/dist/carousel.css";
 
 export default defineComponent({
-	components: {
-	Carousel,
+  components: {
+    Carousel,
     Slide,
     Pagination,
-	},
+  },
 
-	data: ()=>{
-		return{
-			items : [
-				{
-					id: 1,
-					img: require('@/assets/images/a1.jpg'),
-				},
-				{
-					id: 2,
-					img: require('@/assets/images/a2.jpg'),
-				},
-				{
-					id: 3,
-					img: require('@/assets/images/a3.jpg'),
-				},
-				// {
-				// 	id: 4,
-				// 	img: require('@/assets/images/a4.jpg'),
-				// },
-			]
-		}
-	}
-})
+  data: () => {
+    return {
+      items: [
+        {
+          id: 1,
+          img: require("@/assets/images/a1.jpg"),
+        },
+        {
+          id: 2,
+          img: require("@/assets/images/a2.jpg"),
+        },
+        {
+          id: 3,
+          img: require("@/assets/images/a3.jpg"),
+        },
+        // {
+        // 	id: 4,
+        // 	img: require('@/assets/images/a4.jpg'),
+        // },
+      ],
+    };
+  },
+});
 </script>
 
 <template>
-	<Carousel :autoplay="3000" :wrap-around="true">
-
+  <Carousel :autoplay="3000" :wrap-around="true">
     <Slide v-for="item in items" :key="item.id" :item="item">
-		<!-- <div class="carousel__item"> -->
-		<img :src="item.img">
-		<!-- </div> -->
+      <!-- <div class="carousel__item"> -->
+      <img :src="item.img" />
+      <!-- </div> -->
     </Slide>
 
-		<template #addons>
-		<Pagination />
-		<br>
+    <template #addons>
+      <Pagination />
+      <br />
 
-		<!-- <img src="@/assets/images/a4.jpg"> -->
+      <!-- <img src="@/assets/images/a4.jpg"> -->
 
-		<h4 style="color: green; font-weight: bold;"> " 전국 1등 부동산 커뮤니티 "</h4>
-		<p> 언제 어디서든 부동산의 궁금증 해결이 가능한 사이트 !</p>
-		</template>
-	</Carousel>
+      <h4 style="color: green; font-weight: bold">" 전국 1등 부동산 커뮤니티 "</h4>
+      <p>언제 어디서든 부동산의 궁금증 해결이 가능한 사이트 !</p>
+    </template>
+  </Carousel>
 </template>
 
 <style scoped>
-	/* img{
+/* img{
 		display: block; margin: 0px auto;
 	} */
 
-	.carousel__item {
-		min-height: 200px;
-		width: 100%;
-		background-color: var(--vc-clr-primary);
-		color: var(--vc-clr-white);
-		font-size: 20px;
-		border-radius: 8px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
--- vuejs-tailwindcss-portfolio/src/main.js
+++ vuejs-tailwindcss-portfolio/src/main.js
@@ -1,35 +1,35 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import './assets/css/app.css';
-import BackToTop from 'vue-backtotop';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/css/app.css";
+import BackToTop from "vue-backtotop";
 import store from "./store";
 
-import Vuex from 'vuex';
+import Vuex from "vuex";
 
-import BootstrapVue3 from 'bootstrap-vue-3'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import BootstrapVue3 from "bootstrap-vue-3";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
-const feather = require('feather-icons');
+// import loadScript from "vue-plugin-load-script";
+
+const feather = require("feather-icons");
 feather.replace();
 
 createApp(App)
-	.use(router)
-	.use(Vuex)
-	.use(BackToTop)
-	.use(BootstrapVue3)
-	.use(store)
-	.mount('#app');
+  .use(router)
+  .use(Vuex)
+  .use(BackToTop)
+  .use(BootstrapVue3)
+  .use(store)
+  // .use(loadScript)
+  .mount("#app");
 
-const appTheme = localStorage.getItem('theme');
+const appTheme = localStorage.getItem("theme");
 
 // Check what is the active theme and change theme when user clicks on the theme button in header.
-if (
-	appTheme === 'dark' &&
-	document.querySelector('body').classList.contains('app-theme')
-) {
-	document.querySelector('body').classList.add('bg-primary-dark');
+if (appTheme === "dark" && document.querySelector("body").classList.contains("app-theme")) {
+  document.querySelector("body").classList.add("bg-primary-dark");
 } else {
-	document.querySelector('body').classList.add('bg-secondary-light');
+  document.querySelector("body").classList.add("bg-secondary-light");
 }
